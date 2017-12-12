6258b78015e0c93062c9dfc6155851fd235152f6
sandanzuki@aether <kyasshiou@gmail.com>
2017-12-11 19:25:19 -0800

    easy way to add urls to the database

--- service/lookup.py
+++ service/lookup.py
@@ -4,8 +4,6 @@ from flask_api import status
 import redis
 
 app = flask.Flask(__name__)
-
-test = ['www.reddit.com/r/dailyprogrammer','www.stackoverflow.com/questions/41718376/how-to-unit-test-a-flask-restful-api']
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # <hostname_and_port> <original_path_and_query_string>
@@ -16,7 +14,7 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 # this should redirect
 # http://127.0.0.1:5000/urlinfo/1/www.reddit.com:80/r/programmerhumor
 
-@app.route('/urlinfo/1/<hostname_and_port>/', defaults={'original_path_and_query_string': ''})
+@app.route('/urlinfo/1/<hostname_and_port>/', defaults={'original_path_and_query_string': ''}, methods=['GET'])
 @app.route('/urlinfo/1/<hostname_and_port>/<path:original_path_and_query_string>', methods=['GET'])
 def lookup(hostname_and_port, original_path_and_query_string):
     hostname, port = hostname_and_port.split(':')
@@ -33,6 +31,22 @@ def lookup(hostname_and_port, original_path_and_query_string):
         return resp, status.HTTP_302_FOUND
 
     return 'malicious', status.HTTP_400_BAD_REQUEST
+
+@app.route('/urlinfo/1/<hostname_and_port>/', defaults={'original_path_and_query_string': ''}, methods=['POST'])
+@app.route('/urlinfo/1/<hostname_and_port>/<path:original_path_and_query_string>', methods=['POST'])
+def add_url(hostname_and_port, original_path_and_query_string):
+    hostname, port = hostname_and_port.split(':')
+    url = hostname + ':' + port + '/' + original_path_and_query_string
+
+    try:
+        result = r.set(url,1)
+    except:
+        return 'service is down', status.HTTP_503_SERVICE_UNAVAILABLE
+
+    if result is None:
+        return 'could not update DB', status.HTTP_400_BAD_REQUEST
+
+    return '', status.HTTP_200_OK
 
 if __name__ == "__main__":
     app.run(debug=True)
